082f99d098b951b4ec1ed6bcc90d4e5e8206af81
Alex Pimenta <aspepper@hotmail.com>
2023-06-14 16:03:12 -0300

    Implementation of mouse linear movement and window hiding with hotkey (this second one still disabled)

--- InPipiroom/MouseMover.cs
+++ InPipiroom/MouseMover.cs
@@ -2,6 +2,7 @@ using System.Drawing;
 using System.Management;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
+using static System.Windows.Forms.AxHost;
 
 namespace OnPipiroom
 {
@@ -11,6 +12,9 @@ namespace OnPipiroom
         private uint ScreenResolutionWidth = 0;
         private uint ScreenResolutionHeight = 0;
         private readonly uint WindowsTaskBarTall = 72; // Most tall in Pixels
+        private Point CurrentMousePoint = new();
+
+        #region User32 Dynalic Link Library
 
         [Flags]
         public enum MouseEventFlags
@@ -25,7 +29,8 @@ namespace OnPipiroom
             RIGHTUP = 0x00000010
         }
 
-        #region User32 Dynalic Link Library
+        [DllImport("user32.dll")]
+        static extern bool GetCursorPos(ref Point lpPoint);
 
         [DllImport("user32.dll")]
         static extern bool SetCursorPos(int X, int Y);
@@ -65,7 +70,7 @@ namespace OnPipiroom
             }
         }
 
-        static void LeftClick(Point p)
+        void LeftClick(Point p)
         {
             Cursor.Position = p;
             mouse_event((int)(MouseEventFlags.LEFTDOWN), 0, 0, 0, 0);
@@ -73,34 +78,78 @@ namespace OnPipiroom
             mouse_event((int)(MouseEventFlags.LEFTUP), 0, 0, 0, 0);
         }
 
+        void MoveMouse(int toX, int toY, int speed)
+        {
+            GetCursorPos(ref CurrentMousePoint);
+            int deltaX = toX - CurrentMousePoint.X;
+            int deltaY = toY - CurrentMousePoint.Y;
+
+            int steps = Math.Max(Math.Abs(deltaX), Math.Abs(deltaY));
+
+            float stepX = (float)deltaX / steps;
+            float stepY = (float)deltaY / steps;
+
+            float currentX = CurrentMousePoint.X;
+            float currentY = CurrentMousePoint.Y;
+
+            Console.Write($"Mouse moverá de ({CurrentMousePoint.X},{CurrentMousePoint.Y}) para ({toX},{toY}), na velocidade {speed} [steps:{steps},stepY:{stepY},stepX:{stepX}]. ");
+
+            for (int i = 0; i <= steps; i++)
+            {
+                int x = (int)Math.Round(currentX);
+                int y = (int)Math.Round(currentY);
+
+                SetCursorPos(x, y);
+
+                Thread.Sleep(speed);
+
+                currentX += stepX;
+                currentY += stepY;
+            }
+            Console.Write($"-> Out.");
+        }
+
+
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+
+            Random speed = new();
+            Random rndTime = new();
             Random randomMin = new();
+            Random random = new();
+            int toWait = rndTime.Next(5, 15);
+
             while (!stoppingToken.IsCancellationRequested)
             {
                 var currentTime = DateTime.Now;
                 if (currentTime.Hour >= 8 && currentTime.Hour <= 20)
                 {
-                    if ((DateTime.Now - GetLastInputTime()).TotalSeconds >= 15)
+                    if ((DateTime.Now - GetLastInputTime()).TotalSeconds >= toWait)
                     {
-                        Random random = new();
+                        toWait = rndTime.Next(5, 15);
                         int x = random.Next((int)ScreenResolutionWidth);
                         int y = random.Next((int)ScreenResolutionHeight);
-                        SetCursorPos(x, y);
-                        if (random.Next(10) == 5 && (y > WindowsTaskBarTall && y < (ScreenResolutionHeight - WindowsTaskBarTall)))
-                        { LeftClick(new Point(x, y)); }
+                        MoveMouse(x, y, speed.Next(1, 3));
+                        if (random.Next(20) == 5 && (y > WindowsTaskBarTall && y < (ScreenResolutionHeight - (WindowsTaskBarTall * 2))))
+                        { LeftClick(new Point(x, y)); Console.Write(" Clicou!"); }
+                    }
+                    else
+                    {
+                        Console.Write($"Tempo não passou ainda, ultimo movimento {GetLastInputTime()}, send que se passaram {(DateTime.Now - GetLastInputTime()).TotalSeconds}, e a espera é de {toWait}.");
                     }
                     int randomNumber = randomMin.Next(500, 5000);
+                    Console.WriteLine($" Aguardando {randomNumber} Milisegundos.");
                     await Task.Delay(randomNumber, stoppingToken);
                 }
                 else
                 {
+                    Console.WriteLine("Está fora do horário.");
                     await Task.Delay(1000, stoppingToken);
                 }
             }
         }
 
-        private static DateTime GetLastInputTime()
+        static DateTime GetLastInputTime()
         {
             LASTINPUTINFO lastInputInfo = new();
             lastInputInfo.cbSize = (uint)Marshal.SizeOf(lastInputInfo);
--- InPipiroom/Program.cs
+++ InPipiroom/Program.cs
@@ -1,11 +1,8 @@
+using InPipiroom;
 using OnPipiroom;
-//using System.Runtime.InteropServices;
-
-//[DllImport("user32.dll")]
-//static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
-
-//[DllImport("user32.dll")]
-//static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
@@ -14,9 +11,5 @@ IHost host = Host.CreateDefaultBuilder(args)
     })
     .Build();
 
-//IntPtr hWnd = FindWindow(null, Console.Title);
-//if (hWnd != IntPtr.Zero)
-//{
-//    ShowWindow(hWnd, 0); // Esconde o ícone da barra de tarefas
-//}
 host.Run();
+//Application.Run(new ConsoleReceiver());
